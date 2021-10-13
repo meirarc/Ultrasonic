9054fef7a9e152827eb97a892a24c911976c5b9d
Rafael Cabrera <rafaelcabrera@Rafaels-MacBook-Pro.local>
2021-10-13 18:47:20 -0300

    Initial files

---
+++ Ultrasonic.cpp
@@ -0,0 +1,62 @@
+/*
+ * Ultrasonic.cpp - Library for HC-SR04 Ultrasonic Sensing Module.
+ *
+ * Centimeters Divisor
+ * =========== =======
+ *  15.875     26.9029
+ *  46.355     27.6233
+ *  92.075     28.1949
+ * 137.795     28.4717
+ * 183.515     28.5584
+ * 229.235     28.5936
+ * 274.955     28.7194
+ *
+ * Inches      Divisor
+ * ======      =======
+ *   6.25      68.3333
+ *  18.25      70.1633
+ *  36.25      71.6152
+ *  54.25      72.3182
+ *  72.25      72.5384
+ *  90.25      72.6277
+ * 108.25      72.9473
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <Ultrasonic.h>
+
+
+Ultrasonic::Ultrasonic(int tp, int ep)
+    {
+    pinMode(tp, OUTPUT);
+    pinMode(ep, INPUT);
+    _trigPin = tp;
+    _echoPin = ep;
+    _cmDivisor = 27.6233;
+    _inDivisor = 70.1633;
+    }
+
+long Ultrasonic::timing()
+    {
+    digitalWrite(_trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(_trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(_trigPin, LOW);
+    return pulseIn(_echoPin, HIGH);
+    }
+
+float Ultrasonic::convert(long microsec, int metric)
+    {
+    // microsec / 29 / 2;
+    if(metric) return microsec / _cmDivisor / 2.0;  // CM
+    // microsec / 74 / 2;
+    else return microsec / _inDivisor / 2.0;  // IN
+    }
+
+void Ultrasonic::setDivisor(float value, int metric)
+    {
+    if(metric) _cmDivisor = value;
+    else _inDivisor = value;
+    }
---
+++ Ultrasonic.h
@@ -0,0 +1,35 @@
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
+
+#include <Arduino.h>
+#include <stddef.h>
+
+
+typedef struct bufferCtl
+    {
+    float *pBegin;
+    float *pIndex;
+    size_t length;
+    bool filled;
+    } BufCtl;
+
+class Ultrasonic {
+    
+    public:
+        Ultrasonic(int tp, int ep);
+        long timing();
+        float convert(long microsec, int metric);
+        void setDivisor(float value, int metric);
+    
+        static const int IN = 0;
+        static const int CM = 1;
+
+    private:
+        int _trigPin;
+        int _echoPin;
+        float _cmDivisor;
+        float _inDivisor;
+
+};
+
+#endif 
